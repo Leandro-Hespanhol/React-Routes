f890a525a141ed29a71af5a425ed474fb18077a3
Leandro Hespanhol <leandrohespanhol@gmail.com>
2021-02-04 22:00:20 -0300

    rotasfinalizadas

--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -1,6 +1,29 @@
 import './Menu.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Menu = (props) => <aside className="Menu">Componente Menu</aside>;
+const Menu = (props) => (
+  <aside className="Menu">
+    <nav>
+      <ul>
+        <li>
+          <Link to="/Home">Início</Link>
+        </li>
+        <li>
+          <Link to="/param/123">Param #01</Link>
+        </li>
+        <li>
+          <Link to="/param/legal">Param #02</Link>
+        </li>
+        <li>
+          <Link to="/About">Sobre</Link>
+        </li>
+        <li>
+          <Link to="/NãoExiste">NãoExiste</Link>
+        </li>
+      </ul>
+    </nav>
+  </aside>
+);
 
 export default Menu;
---
+++ src/views/examples/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = (props) => (
+  <div className="NotFound">
+    <h1>404</h1>
+    <p>Opsss... Bixa não tem nada aqui não</p>
+  </div>
+);
+export default NotFound;
